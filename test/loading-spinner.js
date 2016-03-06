46dc5bee14404f16240c0ca72f5a89f0233598ec
Ivan Gabriele <ivan.gabriele@gmail.com>
2016-03-06 11:54:08 +0100

    Tests: new tests

--- test/loading-spinner.js
+++ test/loading-spinner.js
@@ -1,48 +1,37 @@
-var loadingSpinner = require('../lib/loading-spinner');
+var assert = require('assert'),
+    loadingSpinner = require('../lib/loading-spinner');
 
-var dary = function() {
-  loadingSpinner.stop();
-  process.stdout.write('DA-RY !');
-};
-
-var legend = function() {
+function test(done, options) {
   process.stdout.write('It\'s gonna be LE-GEN... Wait for it... ');
-  loadingSpinner.start(100, {
-    clearChar: true
-  });
-  setTimeout(dary, 1000);
+
+  if (options) {
+    loadingSpinner.start(100, {
+      clearChar: true
+    });
+  } else {
+    loadingSpinner.start(100);
+  }
+
+  setTimeout(function() {
+    loadingSpinner.stop();
+    process.stdout.write('DA-RY !');
+
+    done();
+  }, 1000);
 };
 
-legend();
-
-/*function test1() {
-  loadingSpinner.start();
-  setTimeout(function() {
-    loadingSpinner.stop();
-    test2();
-  }, 1000);
-}
-
-function test2() {
-  loadingSpinner.changeSequence(['0o0', 'o0o']);
-  loadingSpinner.start();
-  setTimeout(function() {
-    loadingSpinner.stop();
-    test3();
-  }, 1000);
-}
-
-function test3() {
-  loadingSpinner.start(50,{ hideCursor : true });
-  setTimeout(function() {
-    loadingSpinner.stop();
-    loadingSpinner.start(100, { doNotBlock : true });
-  }, 1000);
-}
-
-process.on('exit', function() {
-  loadingSpinner.stop();
-  console.log('Have a nice day');
+describe('LE-GEN-DA-RY', function() {
+  describe('WITHOUT option', function () {
+    it('SHOULD load without error', function(done) { test(done) });
+  });
+  describe('WITH option(s)', function () {
+    it('clearChar: false', function(done) { test(done, { clearChar: false }) });
+    it('clearChar: true', function(done) { test(done, { clearChar: true }) });
+    it('clearLine: false', function(done) { test(done, { clearLine: false }) });
+    it('clearLine: true', function(done) { test(done, { clearLine: true }) });
+    it('doNotBlock: false', function(done) { test(done, { doNotBlock: false }) });
+    it('doNotBlock: true', function(done) { test(done, { doNotBlock: true }) });
+    it('hideCursor: false', function(done) { test(done, { hideCursor: false }) });
+    it('hideCursor: true', function(done) { test(done, { hideCursor: true }) });
+  });
 });
-
-test1();*/
